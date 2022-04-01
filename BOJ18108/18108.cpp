91eee3e7c0be39a38974acd9f9b2f97c5a43be1c
wjoh0315 <65387631+wjoh0315@users.noreply.github.com>
2022-04-01 17:17:33 +0900

    BOJ10926_BOJ18108 Solved

---
+++ BOJ18108/18108.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int bul;
+    cin >> bul;
+    cout << bul - 543 << '\n';
+}
